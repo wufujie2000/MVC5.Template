55c9df9307c3f22ac7b632a65f2b92a6509a5a4c
Muchiachio <aldis.rameika@gmail.com>
2014-03-05 09:58:34 +0200

    Template.Components.Service tests
    - Added menu service tests, just to cover it's code base. Normal tests
    will be added with tests clean up.

---
+++ Tests/Tests/Components/Services/Shared/Sidebar/MenuServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Web;
+using Template.Components.Services;
+using Tests.Helpers;
+
+namespace Template.Tests.Tests.Components.Services.Shared.Sidebar
+{
+    [TestFixture]
+    public class MenuServiceTests
+    {
+        private MenuService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            HttpContext.Current = new HttpContextStub().Context;
+            service = new MenuService();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            HttpContext.Current = null;
+        }
+
+        #region Method: IEnumerable<Menu> GetAuthorizedMenus()
+
+        [Test]
+        public void GetAuthorizedMenus_GetsMenusWhichDoesNeedAuthorization()
+        {
+            var menus = service.GetAuthorizedMenus();
+            var menu = menus.First();
+
+            Assert.AreEqual(1, menus.Count());
+            Assert.AreEqual(null, menu.Area);
+            Assert.AreEqual("Home", menu.Controller);
+            Assert.AreEqual("Index", menu.Action);
+            Assert.AreEqual("menu-icon fa fa-home", menu.IconClass);
+        }
+
+        #endregion
+    }
+}

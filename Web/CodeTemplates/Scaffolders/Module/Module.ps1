2a24dafdf8d9e3b00be4500541adb805aa73f67e
Muchiachio <aldis.rameika@gmail.com>
2014-04-15 19:53:21 +0300

    Project module scaffolding
    - Added support for scaffolding modules without areas.
    - Cleaned up module scaffolding script.

--- Web/CodeTemplates/Scaffolders/Module/Module.ps1
+++ Web/CodeTemplates/Scaffolders/Module/Module.ps1
@@ -1,101 +1,200 @@
 [T4Scaffolding.Scaffolder(Description = "Creates default module controller, service, model, view model and views")][CmdletBinding()]
 param(
-    [parameter(Mandatory = $true)][string]$AreaName,
-    [parameter(Mandatory = $true)][string]$ControllerName,
     [parameter(Mandatory = $true)][string]$ModelName,
+    [parameter(Mandatory = $true)][string]$ControllerName,
+    [parameter(Mandatory = $false)][string]$AreaName,
     [string[]]$TemplateFolders,
     [switch]$Force = $false
 )
 
-$ModelProject = "Objects"
-$ModelNamespace = "Template.Objects"
-$ModelPath = "Models\" + $AreaName + "\" + $ControllerName + "\" + $ModelName
-
-Add-ProjectItemViaTemplate $ModelPath -Template "Controls\Model" `
-    -Model @{ Namespace = $ModelNamespace; Name = $ModelName } `
-    -SuccessMessage "Added model at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ModelProject -CodeLanguage $CodeLanguage -Force:$Force
-
-$ViewProject = "Objects"
-$View = $ModelName + "View"
-$ViewNamespace = "Template.Objects"
-$ViewPath = "Views\" + $AreaName + "\" + $ControllerName + "\" + $View
-
-Add-ProjectItemViaTemplate $ViewPath -Template "Controls\View" `
-    -Model @{ Namespace = $ViewNamespace; Name = $View } `
-    -SuccessMessage "Added view model at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ViewProject -CodeLanguage $CodeLanguage -Force:$Force
-
-$ServiceProject = "Components"
-$Service = $ControllerName + "Service"
-$ServiceNamespace = "Template.Components.Services"
-$ServiceInterface = "I" + $ControllerName + $Service
-$ServicePath = "Services\" + $AreaName + "\" + $ControllerName + "\" + $Service
-$ServiceInterfacePath = "Services\" + $AreaName + "\" + $ControllerName + "\" + $ServiceInterface
-
-Add-ProjectItemViaTemplate $ServiceInterfacePath -Template "Controls\IService" `
-    -Model @{ Namespace = $ServiceNamespace; Name = $ServiceInterface; View = $View } `
-    -SuccessMessage "Added service interface at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ServiceProject -CodeLanguage $CodeLanguage -Force:$Force
-
-Add-ProjectItemViaTemplate $ServicePath -Template "Controls\Service" `
-    -Model @{ Namespace = $ServiceNamespace; Name = $Service; Model = $ModelName; View = $View; Interface = $ServiceInterface } `
-    -SuccessMessage "Added service implementation at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ServiceProject -CodeLanguage $CodeLanguage -Force:$Force
-
-$ControllerProject = "Controllers"
-$Controller = $ControllerName + "Controller"
-$ControllerNamespace = "Template.Controllers." + $AreaName
-$ControllerPath = $AreaName + "\" + $ControllerName + "\" + $Controller
-
-Add-ProjectItemViaTemplate $ControllerPath -Template "Controls\Controller" `
-    -Model @{ Namespace = $ControllerNamespace; Name = $Controller; Service = $ServiceInterface; View = $View } `
-    -SuccessMessage "Added controller at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ControllerProject -CodeLanguage $CodeLanguage -Force:$Force
-    
-$ViewsProject = "Web"
-$ViewsPath = "Views\" + $AreaName + "\" + $ControllerName + "\"
-
-Add-ProjectItemViaTemplate ($ViewsPath + "Index") -Template "Views\Index" `
-    -Model @{ View = $View } `
-    -SuccessMessage "Added index view at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ViewsProject -CodeLanguage $CodeLanguage -Force:$Force
-
-Add-ProjectItemViaTemplate ($ViewsPath + "Create") -Template "Views\Create" `
-    -Model @{ View = $View } `
-    -SuccessMessage "Added create view at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ViewsProject -CodeLanguage $CodeLanguage -Force:$Force
-
-Add-ProjectItemViaTemplate ($ViewsPath + "Details") -Template "Views\Details" `
-    -Model @{ View = $View } `
-    -SuccessMessage "Added details view at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ViewsProject -CodeLanguage $CodeLanguage -Force:$Force
-
-Add-ProjectItemViaTemplate ($ViewsPath + "Edit") -Template "Views\Edit" `
-    -Model @{ View = $View } `
-    -SuccessMessage "Added edit view at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ViewsProject -CodeLanguage $CodeLanguage -Force:$Force
-
-Add-ProjectItemViaTemplate ($ViewsPath + "Delete") -Template "Views\Delete" `
-    -Model @{ View = $View } `
-    -SuccessMessage "Added delete view at {0}" `
-    -TemplateFolders $TemplateFolders -Project $ViewsProject -CodeLanguage $CodeLanguage -Force:$Force
+# Variables
 
 $TestsProject = "Tests"
-$ServiceTests = $Service + "Tests"
+$RazorViewProject = "Web"
+$ObjectsProject = "Objects"
+$ServiceProject = "Components"
+$ControllerProject = "Controllers"
+
+$ViewNamespace = "Template.Objects"
+$ModelNamespace = "Template.Objects"
+$ServiceNamespace = "Template.Components.Services"
+$ControllerNamespace = "Template.Controllers.$AreaName"
 $ServiceTestsNamespace = "Template.Tests.Unit.Components.Services"
-$ServiceTestsPath = "Unit\Components\Services\" + $AreaName + "\" + $ControllerName + "\" + $ServiceTests
+$ControllerTestsNamespace = "Template.Tests.Unit.Controllers.$AreaName"
 
-Add-ProjectItemViaTemplate $ServiceTestsPath -Template "Tests\ServiceTests" `
-    -Model @{ Namespace = $ServiceTestsNamespace; Name = $ServiceTests; Service = $Service; View = $View } `
-    -SuccessMessage "Added tests for service at {0}" `
-    -TemplateFolders $TemplateFolders -Project $TestsProject -CodeLanguage $CodeLanguage -Force:$Force
+$Model = $ModelName
+$View = $ModelName + "View"
+$Service = $ControllerName + "Service"
+$Controller = $ControllerName + "Controller"
+$ServiceInterface = "I" + $ControllerName + "Service"
+$ServiceTests = $ControllerName + "Service" + "Tests"
+$ControllerTests = $ControllerName + "Controller" + "Tests"
 
-$ControllerTests = $Controller + "Tests"
-$ControllerTestsNamespace = "Template.Tests.Unit.Controllers." + $AreaName
-$ControllerTestsPath = "Unit\Controllers\" + $AreaName + "\" + $ControllerName + "\" + $ControllerTests
+If ($AreaName) { $ElementPath = "$AreaName\$ControllerName"; }
+Else { $ElementPath = $ControllerName; }
 
-Add-ProjectItemViaTemplate $ControllerTestsPath -Template "Tests\ControllerTests" `
-    -Model @{ Namespace = $ControllerTestsNamespace; ControllerNamespace = $ControllerNamespace; Name = $ControllerTests; Controller = $Controller; ServiceInterface = $ServiceInterface; View = $View } `
-    -SuccessMessage "Added tests for controller at {0}" `
-    -TemplateFolders $TemplateFolders -Project $TestsProject -CodeLanguage $CodeLanguage -Force:$Force
+$ViewPath = "Views\$ElementPath\$View"
+$RazorViewPath = "Views\$ElementPath\"
+$ControllerPath = "$ElementPath\$Controller"
+$ModelPath = "Models\$ElementPath\$ModelName"
+$ServicePath = "Services\$ElementPath\$Service"
+$ServiceInterfacePath = "Services\$ElementPath\$ServiceInterface"
+$ControllerTestsPath = "Unit\Controllers\$ElementPath\$ControllerTests"
+$ServiceTestsPath = "Unit\Components\Services\$ElementPath\$ServiceTests"
+
+# Controls
+
+Add-ProjectItemViaTemplate $ModelPath `
+    -Template "Controls\Model" `
+    -Model @{ `
+	    Namespace = $ModelNamespace; `
+		Name = $ModelName `
+	} `
+    -SuccessMessage "Added model at $ObjectsProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $ObjectsProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate $ViewPath `
+    -Template "Controls\View" `
+    -Model @{ `
+	  Namespace = $ViewNamespace; `
+	  Name = $View `
+	} `
+    -SuccessMessage "Added view model at $ObjectsProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $ObjectsProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate $ServiceInterfacePath `
+    -Template "Controls\IService" `
+    -Model @{ `
+	    Namespace = $ServiceNamespace; `
+		Name = $ServiceInterface; `
+		View = $View `
+	} `
+    -SuccessMessage "Added service interface at $ServiceProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $ServiceProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate $ServicePath `
+    -Template "Controls\Service" `
+    -Model @{ `
+	    Namespace = $ServiceNamespace; `
+		Interface = $ServiceInterface; `
+		Model = $ModelName; `
+		Name = $Service; `
+		View = $View `
+	} `
+    -SuccessMessage "Added service implementation at $ServiceProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $ServiceProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate $ControllerPath `
+    -Template "Controls\Controller" `
+    -Model @{ `
+	    Namespace = $ControllerNamespace; `
+		Service = $ServiceInterface; `
+		Name = $Controller; `
+		View = $View `
+	} `
+    -SuccessMessage "Added controller at $ControllerProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $ControllerProject `
+	-Force:$Force
+
+# Views
+
+Add-ProjectItemViaTemplate ($RazorViewPath + "Index") `
+    -Template "Views\Index" `
+    -Model @{ `
+	    View = $View `
+	} `
+    -SuccessMessage "Added index view at $RazorViewProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $RazorViewProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate ($RazorViewPath + "Create") `
+    -Template "Views\Create" `
+    -Model @{ `
+	    View = $View `
+	} `
+    -SuccessMessage "Added create view at $RazorViewProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $RazorViewProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate ($RazorViewPath + "Details") `
+    -Template "Views\Details" `
+    -Model @{ `
+	    View = $View `
+	} `
+    -SuccessMessage "Added details view at $RazorViewProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $RazorViewProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate ($RazorViewPath + "Edit") `
+    -Template "Views\Edit" `
+    -Model @{ `
+	    View = $View `
+	} `
+    -SuccessMessage "Added edit view at $RazorViewProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $RazorViewProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate ($RazorViewPath + "Delete") `
+    -Template "Views\Delete" `
+    -Model @{ `
+	    View = $View `
+	} `
+    -SuccessMessage "Added delete view at $RazorViewProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $RazorViewProject `
+	-Force:$Force
+
+# Tests
+
+Add-ProjectItemViaTemplate $ServiceTestsPath `
+    -Template "Tests\ServiceTests" `
+    -Model @{ `
+	    Namespace = $ServiceTestsNamespace; `
+		Name = $ServiceTests; `
+		Service = $Service; `
+		View = $View `
+	} `
+    -SuccessMessage "Added tests for service at $TestsProject\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $TestsProject `
+	-Force:$Force
+
+Add-ProjectItemViaTemplate $ControllerTestsPath `
+    -Template "Tests\ControllerTests" `
+    -Model @{ `
+		ControllerNamespace = $ControllerNamespace; `
+	    Namespace = $ControllerTestsNamespace; `
+		ServiceInterface = $ServiceInterface; `
+		Controller = $Controller; `
+		Name = $ControllerTests; `
+		View = $View `
+	} `
+    -SuccessMessage "Added tests for controller at $ControllerTests\{0}" `
+    -TemplateFolders $TemplateFolders `
+	-CodeLanguage $CodeLanguage `
+	-Project $TestsProject `
+	-Force:$Force

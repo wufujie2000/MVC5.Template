dde113e3d0f6df20e9333a32193b69e50c7507ef
Muchiachio <aldis.rameika@gmail.com>
2014-05-24 14:36:43 +0300

    Required adapter tests
    - Added missing localized property name test.
    - Cleaned up required adapter test class.

--- Tests/Unit/Components/Mvc/Adapters/RequiredAdapterTests.cs
+++ Tests/Unit/Components/Mvc/Adapters/RequiredAdapterTests.cs
@@ -1,7 +1,10 @@
 ﻿using NUnit.Framework;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 using Template.Components.Mvc.Adapters;
+using Template.Objects;
+using Template.Resources;
 using Template.Resources.Shared;
 
 namespace Template.Tests.Unit.Components.Mvc.Adapters
@@ -10,17 +13,27 @@ namespace Template.Tests.Unit.Components.Mvc.Adapters
     public class RequiredAdapterTests
     {
         private RequiredAttribute requiredAttribute;
+        private ModelMetadata metadata;
 
         [SetUp]
         public void SetUp()
         {
             requiredAttribute = new RequiredAttribute();
-            ModelMetadata metadata = new DataAnnotationsModelMetadataProvider()
-                .GetMetadataForProperty(null, typeof(RequiredAdapterModel), "Id");
+            metadata = new DataAnnotationsModelMetadataProvider()
+                .GetMetadataForProperty(null, typeof(RoleView), "Name");
             new RequiredAdapter(metadata, new ControllerContext(), requiredAttribute);
         }
 
         #region Constructor: RequiredAdapter(ModelMetadata metadata, ControllerContext context, RequiredAttribute attribute)
+
+        [Test]
+        public void RequiredAdapter_SetsMetadataDisplayName()
+        {
+            String expected = ResourceProvider.GetPropertyTitle(typeof(RoleView), "Name");
+            String actual = metadata.DisplayName;
+
+            Assert.AreEqual(expected, actual);
+        }
 
         [Test]
         public void RequiredAdapter_SetsAttributeErrorMessageResourceType()

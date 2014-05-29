2da89231e86961f37e6f278af099383a7f028d67
Muchiachio <aldis.rameika@gmail.com>
2014-05-29 13:24:13 +0300

    Bundle configuration clean up
    - Cleaned up bundle registration methods.

--- Web/App_Start/BundleConfig.cs
+++ Web/App_Start/BundleConfig.cs
@@ -11,89 +11,29 @@ namespace Template.Web
         }
         private static void RegisterScripts(BundleCollection bundles)
         {
-            ScriptBundle jQueryScripts = new ScriptBundle("~/Scripts/JQuery/Bundle");
-            jQueryScripts.Include("~/Scripts/JQuery/*.js");
-
-            ScriptBundle sidebarScripts = new ScriptBundle("~/Scripts/Sidebar/Bundle");
-            sidebarScripts.Include("~/Scripts/Sidebar/*.js");
-
-            ScriptBundle bootstrapScripts = new ScriptBundle("~/Scripts/Bootstrap/Bundle");
-            bootstrapScripts.Include("~/Scripts/Bootstrap/*.js");
-
-            ScriptBundle jQueryUIScripts = new ScriptBundle("~/Scripts/JQueryUI/Bundle");
-            jQueryUIScripts.Include("~/Scripts/JQueryUI/*.js");
-
-            ScriptBundle globalizeScripts = new ScriptBundle("~/Scripts/Globalize/Bundle");
-            globalizeScripts.Include("~/Scripts/Globalize/*.js");
-
-            ScriptBundle gridMvcScripts = new ScriptBundle("~/Scripts/GridMvc/Bundle");
-            gridMvcScripts.Include("~/Scripts/GridMvc/*.js");
-
-            ScriptBundle select2Scripts = new ScriptBundle("~/Scripts/Select2/Bundle");
-            select2Scripts.Include("~/Scripts/Select2/*.js");
-
-            ScriptBundle jsTreeScripts = new ScriptBundle("~/Scripts/JsTree/Bundle");
-            jsTreeScripts.Include("~/Scripts/JsTree/*.js");
-
-            ScriptBundle datalistScritps = new ScriptBundle("~/Scripts/Datalist/Bundle");
-            datalistScritps.Include("~/Scripts/Datalist/*.js");
-
-            ScriptBundle loginScripts = new ScriptBundle("~/Scripts/Login/Bundle");
-            loginScripts.Include("~/Scripts/Login/*.js");
-
-            ScriptBundle sharedScripts = new ScriptBundle("~/Scripts/Shared/Bundle");
-            sharedScripts.Include("~/Scripts/Shared/*.js");
-            
-            bundles.Add(jQueryScripts);
-            bundles.Add(sidebarScripts);
-            bundles.Add(bootstrapScripts);
-            bundles.Add(jQueryUIScripts);
-            bundles.Add(globalizeScripts);
-            bundles.Add(gridMvcScripts);
-            bundles.Add(select2Scripts);
-            bundles.Add(jsTreeScripts);
-            bundles.Add(datalistScritps);
-            bundles.Add(loginScripts);
-            bundles.Add(sharedScripts);
+            bundles.Add(new ScriptBundle("~/Scripts/JQuery/Bundle").Include("~/Scripts/JQuery/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Sidebar/Bundle").Include("~/Scripts/Sidebar/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap/Bundle").Include("~/Scripts/Bootstrap/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/JQueryUI/Bundle").Include("~/Scripts/JQueryUI/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Globalize/Bundle").Include("~/Scripts/Globalize/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/GridMvc/Bundle").Include("~/Scripts/GridMvc/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Select2/Bundle").Include("~/Scripts/Select2/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/JsTree/Bundle").Include("~/Scripts/JsTree/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Datalist/Bundle").Include("~/Scripts/Datalist/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Login/Bundle").Include("~/Scripts/Login/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Shared/Bundle").Include("~/Scripts/Shared/*.js"));
         }
         private static void RegisterCss(BundleCollection bundles)
         {
-            StyleBundle jQueryUIStyles = new StyleBundle("~/Content/JQueryUI/Bundle");
-            jQueryUIStyles.Include("~/Content/JQueryUI/*.css");
-
-            StyleBundle bootstrapStyles = new StyleBundle("~/Content/Bootstrap/Bundle");
-            bootstrapStyles.Include("~/Content/Bootstrap/*.css");
-
-            StyleBundle fontAwesomeStyles = new StyleBundle("~/Content/FontAwesome/Bundle");
-            fontAwesomeStyles.Include("~/Content/FontAwesome/*.css");
-
-            StyleBundle gridmvcStyles = new StyleBundle("~/Content/GridMvc/Bundle");
-            gridmvcStyles.Include("~/Content/GridMvc/*.css");
-
-            StyleBundle select2Styles = new StyleBundle("~/Content/Select2/Bundle");
-            select2Styles.Include("~/Content/Select2/*.css");
-
-            StyleBundle jsTreeStyles = new StyleBundle("~/Content/JsTree/Bundle");
-            jsTreeStyles.Include("~/Content/JsTree/*.css");
-
-            StyleBundle datalistStyles = new StyleBundle("~/Content/Datalist/Bundle");
-            datalistStyles.Include("~/Content/Datalist/*.css");
-
-            StyleBundle loginStyles = new StyleBundle("~/Content/Login/Bundle");
-            loginStyles.Include("~/Content/Login/*.css");
-
-            StyleBundle sharedStyles = new StyleBundle("~/Content/Shared/Bundle");
-            sharedStyles.Include("~/Content/Shared/*.css");
-
-            bundles.Add(jQueryUIStyles);
-            bundles.Add(bootstrapStyles);
-            bundles.Add(fontAwesomeStyles);
-            bundles.Add(gridmvcStyles);
-            bundles.Add(select2Styles);
-            bundles.Add(jsTreeStyles);
-            bundles.Add(datalistStyles);
-            bundles.Add(loginStyles);
-            bundles.Add(sharedStyles);
+            bundles.Add(new StyleBundle("~/Content/JQueryUI/Bundle").Include("~/Content/JQueryUI/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Bootstrap/Bundle").Include("~/Content/Bootstrap/*.css"));
+            bundles.Add(new StyleBundle("~/Content/FontAwesome/Bundle").Include("~/Content/FontAwesome/*.css"));
+            bundles.Add(new StyleBundle("~/Content/GridMvc/Bundle").Include("~/Content/GridMvc/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Select2/Bundle").Include("~/Content/Select2/*.css"));
+            bundles.Add(new StyleBundle("~/Content/JsTree/Bundle").Include("~/Content/JsTree/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Datalist/Bundle").Include("~/Content/Datalist/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Login/Bundle").Include("~/Content/Login/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Shared/Bundle").Include("~/Content/Shared/*.css"));
         }
     }
 }

6143d27a994ef1771b845bccb1736ebc08541e12
Muchiachio <aldis.rameika@gmail.com>
2014-05-31 19:28:37 +0300

    MvcApplcation tests
    - Removed mvc application tests because they would still be untestable.

--- Tests/Unit/Web/MvcApplicationTests.cs
+++ Tests/Unit/Web/MvcApplicationTests.cs
@@ -1,14 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
-using System.Web.Mvc;
-using Template.Components.Mvc.Binders;
-using Template.Components.Mvc.Providers;
-using Template.Components.Security;
 using Template.Web;
-using Template.Web.DependencyInjection;
 
 namespace Template.Tests.Unit.Web
 {
@@ -41,105 +35,6 @@ namespace Template.Tests.Unit.Web
             String actual = MvcApplication.Version;
             
             Assert.AreEqual(expected, actual);
-        }
-
-        #endregion
-
-        #region Application_Start()
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersModelMetadataProvider()
-        {
-            application.Application_Start();
-
-            Assert.That(ModelMetadataProviders.Current, Is.InstanceOf<DisplayNameMetadataProvider>());
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersDependencyResolver()
-        {
-            application.Application_Start();
-
-
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_UnregistersClientDataTypeModelValidatorProvider()
-        {
-            application.Application_Start();
-
-            Assert.IsFalse(ModelValidatorProviders.Providers.Any(provider => provider.GetType() == typeof(ClientDataTypeModelValidatorProvider)));
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersDataTypeValidator()
-        {
-            application.Application_Start();
-
-            Assert.That(ModelValidatorProviders.Providers.First(), Is.InstanceOf<DataTypeValidatorProvider>());
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersRoleProvider()
-        {
-            application.Application_Start();
-
-            IRoleProvider expected = DependencyContainer.Resolve<IRoleProvider>();
-            IRoleProvider actual = RoleProviderFactory.Instance;
-
-            Assert.AreSame(expected, actual);
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersTrimmingModelBinder()
-        {
-            application.Application_Start();
-
-            Assert.IsTrue(ModelBinders.Binders.Any(binder =>
-                binder.Key == typeof(String) &&
-                binder.Value.GetType() == typeof(TrimmingModelBinder)));
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersViewEngine()
-        {
-            application.Application_Start();
-
-
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersAdapters()
-        {
-            application.Application_Start();
-
-
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersBundles()
-        {
-            application.Application_Start();
-
-
-        }
-
-        [Test]
-        [Ignore]
-        public void Application_Start_RegistersRoutes()
-        {
-            application.Application_Start();
-
-
         }
 
         #endregion

1bedc04357b5b5cde7b7f6f8ed5fbfe1cf95a156
Muchiachio <aldis.rameika@gmail.com>
2014-06-01 16:20:38 +0300

    Site map
    - Added site map file.

--- Web/Todo.cs
+++ Web/Todo.cs
@@ -2,10 +2,7 @@
 {
     internal sealed class Todo
     {
-        // TODO: Add git hook for increasing version
         // TODO: Try to add custom regex validation attribute for client side validation
-
         // TODO: Test bundles for actual search patterns
-        // TODO: Add tests for application start?
     }
 }

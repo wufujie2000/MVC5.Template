eaba5971f2d21753fbc62391c5f13dca1b0102b3
Muchiachio <aldis.rameika@gmail.com>
2014-06-24 11:18:51 +0300

    Clear fix support for all browsers
    - Added clear fix support for IE6+ and other browsers.

--- Web/Todo.cs
+++ Web/Todo.cs
@@ -2,24 +2,24 @@
 {
     internal sealed class Todo
     {
-        // TODO: Add project renaming utility
-        // TODO: Rename tests, to more readable form
-        // TODO: Try combining profile/accounts/auth services
-        // TODO: Make services and other program module naming consistent with one another
+        // TODO: Try adding repository.insert(View) method
         // TODO: Disconnect deleted users from home controller
+        // TODO: Fix email validation styling in registration form
+        // TODO: Disable autocomplete from password fields, exept login
         // TODO: Hide datalist controller's getdata method from public access
         // TODO: Mock all protected controller methods which are used in tests
-        // TODO: Try adding repository.insert(View) method
         // TODO: Wrap up code in extensions like auth extensions having duplicate code
-        // TODO: Add better clearfixes
-        // TODO: Rename resources assembly or it's resources resources files and then change Template.Resources.Shared.Resources occurences
-        // TODO: Add protection from registering two accounts with the same username/email at the same time
         // TODO: Removed Add...Message() methods from services and move them to controller
-        // TODO: Remove usage of home service and replace it with accounts service ~
-        // TODO: Disable autocomplete from password fields, exept login
-        // TODO: Fix email validation styling in registration form
+
+        // TODO: Try combining profile/accounts/auth services
         // TODO: Lower password security to 8 symbols
-        // TODO: Make attributed or special input fields not editable by css/js
+        // TODO: Remove usage of home service and replace it with accounts service ~
         // TODO: Add password recovery functionality
+        // TODO: Make attributed or special input fields not editable by css/js
+        // TODO: Add protection from registering two accounts with the same username/email at the same time
+        // TODO: Make services and other program module naming consistent with one another
+        // TODO: Rename tests, to more readable form
+        // TODO: Rename resources assembly or it's resources resources files and then change Template.Resources.Shared.Resources occurences
+        // TODO: Add project renaming utility
     }
 }

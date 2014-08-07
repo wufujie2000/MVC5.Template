64b4511a7e9ef429e0b53fb4301427df32d2a47b
Muchiachio <aldis.rameika@gmail.com>
2014-08-07 19:28:09 +0300

    No data seeding for testing context
    - Removed unnecessary data seeding method from testing context.

--- Tests/Data/Migrations/Configuration.cs
+++ Tests/Data/Migrations/Configuration.cs
@@ -1,8 +1,6 @@
 namespace MvcTemplate.Tests.Data.Migrations
 {
     using MvcTemplate.Tests.Data;
-    using MvcTemplate.Tests.Objects;
-    using System;
     using System.Data.Entity.Migrations;
     using System.Diagnostics.CodeAnalysis;
 
@@ -13,19 +11,6 @@ namespace MvcTemplate.Tests.Data.Migrations
         {
             MigrationsDirectory = "Data\\Migrations";
             ContextKey = "MvcTemplate.Tests.Data";
-        }
-
-        protected override void Seed(TestingContext context)
-        {
-            for (Int32 i = 1; i <= 10; i++)
-                if (context.Set<TestModel>().Find(i.ToString()) == null)
-                    context.Set<TestModel>().Add(new TestModel()
-                    {
-                        Id = i.ToString(),
-                        Text = "Text" + i.ToString()
-                    });
-
-            context.SaveChanges();
         }
     }
 }

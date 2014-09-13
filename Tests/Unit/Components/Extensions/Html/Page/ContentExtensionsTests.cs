c50c3dad533935080771b2366b64d9ee2d08daca
Muchiachio <aldis.rameika@gmail.com>
2014-09-13 14:27:06 +0300

    Custom styles rendering
    - Added support for controller specific style rendering in each
    controller's page.

---
+++ Tests/Unit/Components/Extensions/Html/Page/ContentExtensionsTests.cs
@@ -0,0 +1,108 @@
+﻿using MvcTemplate.Components.Extensions.Html;
+using MvcTemplate.Tests.Helpers;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace MvcTemplate.Tests.Unit.Components.Extensions.Html
+{
+    [TestFixture]
+    public class ContentExtensionsTests
+    {
+        private UrlHelper urlHelper;
+        private HtmlHelper html;
+        private String path;
+
+        [SetUp]
+        public void SetUp()
+        {
+            path = "ContentExtensionsTests.txt";
+            File.WriteAllText(path, String.Empty);
+
+            html = HtmlHelperFactory.CreateHtmlHelper();
+            urlHelper = new UrlHelper(html.ViewContext.RequestContext);
+        }
+
+        #region Extension method: RenderControllerScript(this HtmlHelper html)
+
+        [Test]
+        public void RenderControllerScript_RendersControllerScriptsWithArea()
+        {
+            String scriptSrc = urlHelper.Content("~/Scripts/Shared/administration/accounts/accounts.js");
+            html.ViewContext.HttpContext.Server.MapPath(scriptSrc).Returns(path);
+
+            String expected = String.Format("<script src=\"{0}\"></script>", scriptSrc);
+            String actual = html.RenderControllerScript().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RenderControllerScript_RendersControllerScriptsWithoutArea()
+        {
+            String scriptSrc = urlHelper.Content("~/Scripts/Shared/accounts/accounts.js");
+            html.ViewContext.HttpContext.Server.MapPath(scriptSrc).Returns(path);
+            html.ViewContext.RouteData.Values["Area"] = null;
+
+            String expected = String.Format("<script src=\"{0}\"></script>", scriptSrc);
+            String actual = html.RenderControllerScript().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RenderControllerScript_DoesNotRendersNotExistingScripts()
+        {
+            html.ViewContext.HttpContext.Server.MapPath(Arg.Any<String>()).Returns(String.Empty);
+
+            String actual = html.RenderControllerScript().ToString();
+            String expected = String.Empty;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+        #region Extension method: RenderControllerStyle(this HtmlHelper html)
+
+        [Test]
+        public void RenderControllerStyle_RendersControllerStylesWithArea()
+        {
+            String styleRef = urlHelper.Content("~/Content/Shared/administration/accounts/accounts.css");
+            html.ViewContext.HttpContext.Server.MapPath(styleRef).Returns(path);
+
+            String expected = String.Format("<link href=\"{0}\" rel=\"stylesheet\"></link>", styleRef);
+            String actual = html.RenderControllerStyle().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RenderControllerStyle_RendersControllerStylesWithoutArea()
+        {
+            String styleRef = urlHelper.Content("~/Content/Shared/accounts/accounts.css");
+            html.ViewContext.HttpContext.Server.MapPath(styleRef).Returns(path);
+            html.ViewContext.RouteData.Values["Area"] = null;
+
+            String expected = String.Format("<link href=\"{0}\" rel=\"stylesheet\"></link>", styleRef);
+            String actual = html.RenderControllerStyle().ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RenderControllerStyle_DoesNotRendersNotExistingStyles()
+        {
+            html.ViewContext.HttpContext.Server.MapPath(Arg.Any<String>()).Returns(String.Empty);
+
+            String actual = html.RenderControllerStyle().ToString();
+            String expected = String.Empty;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+    }
+}
---
+++ Web/Components/Extensions/Html/Page/ContentExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcTemplate.Components.Extensions.Html
+{
+    public static class ContentExtensions
+    {
+        public static MvcHtmlString RenderControllerScript(this HtmlHelper html)
+        {
+            RouteValueDictionary routeValues = html.ViewContext.RouteData.Values;
+            UrlHelper urlHelper = new UrlHelper(html.ViewContext.RequestContext);
+            String controller = routeValues["controller"].ToString();
+            String scriptDir = controller;
+
+            if (routeValues["Area"] != null)
+                scriptDir = String.Format("{0}/{1}", routeValues["Area"], scriptDir);
+
+            String virtualPath = urlHelper.Content(String.Format("~/Scripts/Shared/{0}/{1}.js", scriptDir, controller.ToLower()));
+            String physicalPath = html.ViewContext.RequestContext.HttpContext.Server.MapPath(virtualPath);
+            if (!File.Exists(physicalPath)) return new MvcHtmlString(String.Empty);
+
+            TagBuilder script = new TagBuilder("script");
+            script.MergeAttribute("src", virtualPath);
+
+            return new MvcHtmlString(script.ToString());
+        }
+        public static MvcHtmlString RenderControllerStyle(this HtmlHelper html)
+        {
+            RouteValueDictionary routeValues = html.ViewContext.RouteData.Values;
+            UrlHelper urlHelper = new UrlHelper(html.ViewContext.RequestContext);
+            String controller = routeValues["controller"].ToString();
+            String styleDir = controller;
+
+            if (routeValues["Area"] != null)
+                styleDir = String.Format("{0}/{1}", routeValues["Area"], styleDir);
+
+            String virtualPath = urlHelper.Content(String.Format("~/Content/Shared/{0}/{1}.css", styleDir, controller.ToLower()));
+            String physicalPath = html.ViewContext.RequestContext.HttpContext.Server.MapPath(virtualPath);
+            if (!File.Exists(physicalPath)) return new MvcHtmlString(String.Empty);
+
+            TagBuilder link = new TagBuilder("link");
+            link.MergeAttribute("href", virtualPath);
+            link.MergeAttribute("rel", "stylesheet");
+
+            return new MvcHtmlString(link.ToString());
+        }
+    }
+}

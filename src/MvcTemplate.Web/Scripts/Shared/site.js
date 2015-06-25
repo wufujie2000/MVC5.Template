c36515ad8ec60ab4363148d45407ecc37b7d7bfa
Muchiachio <aldis.rameika@gmail.com>
2015-06-25 13:51:50 +0300

    First text input focus
    - Fixed, date and date time picker's being focused, instead of text
    inputs, resulting in date picker being opened.

--- src/MvcTemplate.Web/Scripts/Shared/site.js
+++ src/MvcTemplate.Web/Scripts/Shared/site.js
@@ -193,12 +193,12 @@
 
 // Input focus binding
 (function () {
-    var invalidInput = $('.content-container .input-validation-error:visible:not([readonly]):first');
+    var invalidInput = $('.content-container .input-validation-error:visible:not([readonly],.datepicker,.datetimepicker):first');
     if (invalidInput.length > 0) {
         invalidInput.focus();
         invalidInput.val(invalidInput.val());
     } else {
-        var input = $('.content-container input:text:visible:not([readonly]):first');
+        var input = $('.content-container input:text:visible:not([readonly],.datepicker,.datetimepicker):first');
         if (input.length > 0) {
             input.focus();
             input.val(input.val());

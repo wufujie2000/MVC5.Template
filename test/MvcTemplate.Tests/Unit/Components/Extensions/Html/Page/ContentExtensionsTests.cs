558230826f51f35763e1be5bdef8de0f24e5bcc7
Muchiachio <aldis.rameika@gmail.com>
2015-11-18 20:50:44 +0200

    Cached content extensions tests
    - Added cached content extensions tests, to fully cover content
    extensions code base.

--- test/MvcTemplate.Tests/Unit/Components/Extensions/Html/Page/ContentExtensionsTests.cs
+++ test/MvcTemplate.Tests/Unit/Components/Extensions/Html/Page/ContentExtensionsTests.cs
@@ -50,6 +50,21 @@ namespace MvcTemplate.Tests.Unit.Components.Extensions.Html
         }
 
         [Fact]
+        public void RenderControllerScript_CachesResultPath()
+        {
+            String scriptSrc = urlHelper.Content("~/Scripts/Shared/administration/accounts/accounts.js");
+            html.ViewContext.HttpContext.Server.MapPath(scriptSrc).Returns(path);
+
+            html.RenderControllerScript();
+            html.ViewContext.HttpContext.Server.MapPath(scriptSrc).Returns("Test");
+
+            String expected = String.Format("<script src=\"{0}\"></script>", scriptSrc);
+            String actual = html.RenderControllerScript().ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
         public void RenderControllerScript_NoScripts_DoesNotRender()
         {
             html.ViewContext.HttpContext.Server.MapPath(Arg.Any<String>()).Returns("");
@@ -83,6 +98,21 @@ namespace MvcTemplate.Tests.Unit.Components.Extensions.Html
             String styleRef = urlHelper.Content("~/Content/Shared/accounts/accounts.css");
             html.ViewContext.HttpContext.Server.MapPath(styleRef).Returns(path);
             html.ViewContext.RouteData.Values["Area"] = null;
+
+            String expected = String.Format("<link href=\"{0}\" rel=\"stylesheet\"></link>", styleRef);
+            String actual = html.RenderControllerStyle().ToString();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RenderControllerStyle_CachesResultPath()
+        {
+            String styleRef = urlHelper.Content("~/Content/Shared/administration/accounts/accounts.css");
+            html.ViewContext.HttpContext.Server.MapPath(styleRef).Returns(path);
+
+            html.RenderControllerStyle();
+            html.ViewContext.HttpContext.Server.MapPath(styleRef).Returns("Test");
 
             String expected = String.Format("<link href=\"{0}\" rel=\"stylesheet\"></link>", styleRef);
             String actual = html.RenderControllerStyle().ToString();
